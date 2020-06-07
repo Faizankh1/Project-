bff3f1823adb224eb5b35bfae79ff952dc902680
Faizankh1 <faizyfaizan817@gmail.com>
2020-06-07 16:54:50 +0500

    ready

---
+++ jquery.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
